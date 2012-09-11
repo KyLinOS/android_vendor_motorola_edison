b6246f995f0dd8855e47779ad27667e2ded40350
Hashcode <hashcode0f@gmail.com>
2012-09-10 23:00:54 -0700

    remove stock camera lib.  use source instead
    Change-Id: I428ba831d352b82a736d800fd005e99c8b38f298

--- edison-vendor-stock-camera.mk
+++ edison-vendor-stock-camera.mk
@@ -1,9 +1,9 @@
 VENDOR_BLOB_FOLDER := vendor/motorola/edison/proprietary
 
 # system/lib
-PRODUCT_COPY_FILES += \
-    $(VENDOR_BLOB_FOLDER)/lib/hw/camera.omap4.so:/system/lib/hw/camera.omap4.so \
-    $(VENDOR_BLOB_FOLDER)/lib/libpostcapimgproc_intf.so:/system/lib/libpostcapimgproc_intf.so \
+#PRODUCT_COPY_FILES += \
+#    $(VENDOR_BLOB_FOLDER)/lib/hw/camera.omap4.so:/system/lib/hw/camera.omap4.so \
+#    $(VENDOR_BLOB_FOLDER)/lib/libpostcapimgproc_intf.so:/system/lib/libpostcapimgproc_intf.so \
 
 # system/etc/omapcam
 PRODUCT_COPY_FILES += \
